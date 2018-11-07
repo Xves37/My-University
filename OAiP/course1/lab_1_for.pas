b0b47945a6c090d0bffdd437d9bf36d03f7ad185
Daniil Khvesiuk <danik37h@gmail.com>
2018-11-07 14:05:52 +0300

    test

---
+++ OAiP/course1/lab_1_for.pas
@@ -0,0 +1,13 @@
+﻿program lab_1_for;
+var n,m,i,sum: integer;
+begin
+  readln(n);
+  readln(m);
+  sum:=0;
+  
+  for i:= m to n do
+   if (i mod 2 = 0) then sum:=sum+i;
+  {end for n > m}
+  
+  writeln(sum);
+end.
